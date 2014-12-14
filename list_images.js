3cdb188d3640526d7bce4a469ee91a28de17ff07
solarfl4re <solarfl4re@users.noreply.github.com>
2014-12-13 22:15:59 -0900

    Update list_images.js

--- list_images.js
+++ list_images.js
@@ -1,58 +1,58 @@
 function listImages() {
-	function unicodeToString(string) {
-		var x = string;
-		var r = /\\u([\d\w]{4})/gi;
-		x = x.replace(r, function (match, grp) {
-			return String.fromCharCode(parseInt(grp, 16)); } );
-		x = unescape(x);
-		return x;
-	}
+    function unicodeToString(string) {
+        var x = string;
+        var r = /\\u([\d\w]{4})/gi;
+        x = x.replace(r, function (match, grp) {
+            return String.fromCharCode(parseInt(grp, 16)); } );
+        x = unescape(x);
+        return x;
+    }
 
-	var imageScript = document.evaluate('//div[@id="JSDF"]/script[4]', document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null );
+    var imageScript = document.evaluate('//div[@id="JSDF"]/script[4]', document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null );
 
-	var imageScript = imageScript.singleNodeValue.innerHTML;
+    var imageScript = imageScript.singleNodeValue.innerHTML;
 
-	var images = [];
+    var images = [];
 
-	var image_list = document.getElementsByClassName('lst icon')[0];
-	var num_images = image_list.getElementsByTagName('li').length;
+    var image_list = document.getElementsByClassName('lst icon')[0];
+    var num_images = image_list.getElementsByTagName('li').length;
 
-	current_pos = 0
+    current_pos = 0
 
-	for (i = 0; i < num_images; i++) {
-		var url_start = imageScript.indexOf('maxImageUrl', current_pos);
-		if (url_start == -1) {
-			break;
-			}
+    for (i = 0; i < num_images; i++) {
+        var url_start = imageScript.indexOf('maxImageUrl', current_pos);
+        if (url_start == -1) {
+            break;
+            }
 
-		var url_end = imageScript.indexOf(',', url_start);
+        var url_end = imageScript.indexOf(',', url_start);
 
-		images[images.length] = unicodeToString(imageScript.substring(url_start + 14, url_end - 1));
+        images[images.length] = unicodeToString(imageScript.substring(url_start + 14, url_end - 1));
 
-		current_pos = url_end
-	}
+        current_pos = url_end
+    }
 
-	function getTitle() {
-		var end_pos = document.title.indexOf('|');
-		var title = document.title.substring(0, end_pos - 1);
-		return title;
-	}
+    function getTitle() {
+        var end_pos = document.title.indexOf('|');
+        var title = document.title.substring(0, end_pos - 1);
+        return title;
+    }
 
-	var img_div = document.createElement('div')
-	for (image in images) {
-		// var link_tag = document.createElement('a');
-		// link_tag.href = images[image];
-		// link_tag.download = getTitle() + ' ' + (image + 1) + ' of ' + (images.length + 1) + '.jpg';
-		// link_tag.innerText = 'Download Image';
-		var img_tag = document.createElement('img');
-		img_tag.style = 'width: 600px; height: auto;';
-		img_tag.src = images[image];
-		// img_div.appendChild(link_tag);
-		img_div.appendChild(img_tag);
-		img_div.appendChild(document.createElement('br'));
-		
-	}
-	var body = document.getElementsByTagName('body')[0];
-	body.insertBefore(img_div, body.firstChild);
+    var img_div = document.createElement('div')
+    for (image in images) {
+        // var link_tag = document.createElement('a');
+        // link_tag.href = images[image];
+        // link_tag.download = getTitle() + ' ' + (image + 1) + ' of ' + (images.length + 1) + '.jpg';
+        // link_tag.innerText = 'Download Image';
+        var img_tag = document.createElement('img');
+        img_tag.style = 'width: 600px; height: auto;';
+        img_tag.src = images[image];
+        // img_div.appendChild(link_tag);
+        img_div.appendChild(img_tag);
+        img_div.appendChild(document.createElement('br'));
+        
+    }
+    var body = document.getElementsByTagName('body')[0];
+    body.insertBefore(img_div, body.firstChild);
 }
 listImages();
